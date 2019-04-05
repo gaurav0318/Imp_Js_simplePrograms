4831a6e313a47b4a5a5e3cd5f4d13af05e685acd
gaurav0318 <iamgauravnandi@gmail.com>
2019-04-05 11:13:25 +0530

    concatenation of 2 strings

---
+++ Concatenate_2_strings_except_firstCharacter.js
@@ -0,0 +1,10 @@
+function concatenate(str1, str2){
+	str1 = str1.substring(1, str1.length);
+	str2 = str2.substring(1, str2.length);
+	return str1 + str2;
+}
+
+console.log(concatenate("Madhya","Pradesh"));
+console.log(conccatenate("West","bengal"));
+
+
