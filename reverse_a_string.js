a4ae88f12fe58898132d6fb2fefd33ea9b2e7590
gaurav0318 <iamgauravnandi@gmail.com>
2019-04-05 11:48:46 +0530

    reversing a string

---
+++ reverse_a_string.js
@@ -0,0 +1,6 @@
+var str ="jQuery";
+str = str.split("");
+str = str.reverse();
+str = str.join("");
+console.log(str);
+
---
+++ string_swapping.js
@@ -0,0 +1,11 @@
+function reverseString(str){
+	const arr = [gaurav]
+let reverse="";
+
+while(arr.length){
+	reverse = reverse + arr.pop()
+}
+
+return reverse
+}
+
