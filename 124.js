96dc3adbcc03e8182bac5046f6f58228a4ee6dc7
gaurav0318 <iamgauravnandi@gmail.com>
2019-04-05 13:04:17 +0530

    Sorted pair & NOR logic

---
+++ 124.js
@@ -0,0 +1,9 @@
+function testing_NORlogic(a,b){
+	return(!a && !b)
+}
+
+console.log(testing_NORlogic(true, false));
+console.log(testing_NORlogic(true, false));
+console.log(testing_NORlogic(true, true));
+console.log(testing_NORlogic(false,false));
+
